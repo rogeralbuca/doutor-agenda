a8b5a4bfc9f4cf245f4a5d26250f007b4be3d753
rogeralbuca <albuquerque.roger@gmail.com>
2025-05-27 20:48:13 -0300

    feat: integrate UI dialog component and create clinic form

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,31 @@
-const DashboardPage = () => {
+import { eq } from "drizzle-orm";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { db } from "@/db";
+import { usersToClinicsTable } from "@/db/schema";
+import { auth } from "@/lib/auth";
+import SignOutButton from "./components/sign-out-button";
+
+const DashboardPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+  const clinics = await db.query.usersToClinicsTable.findMany({
+    where: eq(usersToClinicsTable.userId, session.user.id),
+  });
+  if (clinics.length === 0) {
+    redirect("/clinic-form");
+  }
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
-      <h1 className="mb-4 text-4xl font-bold">Welcome to My Dashboard</h1>
-      <p className="text-lg text-gray-700">
-        This is a simple Next.js app with Tailwind CSS.
-      </p>
+    <div>
+      <h1>Dashboard</h1>
+      <h1>{session?.user?.name}</h1>
+      <h1>{session?.user?.email}</h1>
+      <SignOutButton />
     </div>
   );
 };
