a3a7caa06bc3430a11953a665a50d0a20d162fbc
rogeralbuca <albuquerque.roger@gmail.com>
2025-05-27 20:12:08 -0300

    feat: add sign up and login

---
+++ src/app/authentication/page.tsx
@@ -0,0 +1,26 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import LoginForm from "./components/login-form";
+import SignUpForm from "./components/sign-up-form";
+
+const AuthenticationPage = () => {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="flex h-screen w-screen items-center justify-center">
+        <Tabs defaultValue="login" className="w-[400px]">
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="login">Login</TabsTrigger>
+            <TabsTrigger value="register">Criar conta</TabsTrigger>
+          </TabsList>
+          <TabsContent value="login">
+            <LoginForm />
+          </TabsContent>
+          <TabsContent value="register">
+            <SignUpForm />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </div>
+  );
+};
+
+export default AuthenticationPage;
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+const DashboardPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
+      <h1 className="mb-4 text-4xl font-bold">Welcome to My Dashboard</h1>
+      <p className="text-lg text-gray-700">
+        This is a simple Next.js app with Tailwind CSS.
+      </p>
+    </div>
+  );
+};
+
+export default DashboardPage;
